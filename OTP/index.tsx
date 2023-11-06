a067a90f845e513ef0562d0261201feed486e5d5
Dush <dushanthimadhushika3@gmail.com>
2023-11-06 11:02:22 +0530

    Update index.tsx

--- OTP/index.tsx
+++ OTP/index.tsx
@@ -1,11 +1,4 @@
-/*
- * File: index.tsx
- * Project: MT Cloud Portal
- * Author: Dilum Sanjaya (dranasinghe@mitrai.com)
- * File Created: 22 March 2022 12:00 pm
- * Module: MT Cloud Portal Frontend
- * Copyright 2022 - 2022 Mitra Innovation Ltd.
- */
+
 import type { PropsStepper } from '@mtcloud/globals/types';
 import OTPInput from '@mtcloud/ui/atoms/OTPField';
 import Timer from '@mtcloud/ui/atoms/OTPField/Timer';
